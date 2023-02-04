e52cfa41c4b581d7963661482fcbb8d6f07e82ad
Ekrar Efaz <ekrar@Ekrars-MacBook-Air.local>
2023-02-04 23:22:28 +1100

    display vue pages through django

--- backend/mysite/urls.py
+++ backend/mysite/urls.py
@@ -20,9 +20,4 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('theperfectlandlord/', include('thePerfectLandlord.urls')),
-    path('api/', include('api.urls')),
-    path('accounts/login/',
-        auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('accounts/logout/',
-        auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
--- backend/thePerfectLandlord/urls.py
+++ backend/thePerfectLandlord/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 
 from . import views
+from thePerfectLandlord import views as site_views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('myproperties', views.myproperties, name="myproperties"),
-    path('property/<int:id>', views.property, name="property"),
-    path('property/<int:id>/details', views.propertydetails, name="propertydetails"),
-    path('property/<int:id>/details/update', views.propertyupdate, name="propertyupdate"),
+    path('', site_views.vue_test),
 ]
